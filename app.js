bbbc37bfe84cb4a25d287c1905d462325f42fe59
nikitaaZ <nikitafox5@gmail.com>
2018-11-29 20:04:31 +0300

    It's alive

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 let FIELD = [];
 const HTML = document.getElementsByClassName("app")[0];
 const GRID_LENGTH = 7;
-let PLAYER_POSITION = [0,0]
+let PLAYER_POSITION = [1,1]
 
-function Init_Grid(grid_length = 7, player_pos = [0,0]){
+let Init_Grid = (grid_length = 7, player_pos = [0,0]) => {
     for(let i = 0; i < grid__length; i++){
         FIELD[i] = [];
         for(let j = 0; j < grid_length; j++){
@@ -17,8 +17,7 @@ function Init_Grid(grid_length = 7, player_pos = [0,0]){
     }
 }
 
-
-function Draw_Grid(grid_length = 7){
+let Draw_Grid = (grid_length = 7) => {
     for(let i = 0; i < grid_length; i++){
         for(let j = 0; j < grid_length; j++){
             HTML.innerHTML += '<div class="grid-element"></div>';
@@ -26,13 +25,63 @@ function Draw_Grid(grid_length = 7){
     }
 }
 
-function Draw_Player(player_pos = [0,0]){
+let Draw_Player = (player_pos = [0,0]) => {
     let position = player_pos[0]*7 + player_pos[1]*1;
     let cell = document.getElementsByClassName("grid-element")[position];
-    cell.innerHTML += '<img src="person.png">';
+    let current_player = document.getElementsByClassName("player")[0];
+    if(current_player){
+        current_player.parentNode.removeChild(current_player);
+    }
+    cell.innerHTML += '<img class="player" src="person.png">';
     
 }
 
+let Goleft = () =>{
+    if(PLAYER_POSITION[1] > 0){
+        --PLAYER_POSITION[1];
+    }
+    
+}
+
+let Goup = () =>{
+    if(PLAYER_POSITION[0] > 0){
+        --PLAYER_POSITION[0];
+    }
+    
+}
+
+let Godown = () =>{
+    if(PLAYER_POSITION[0] < GRID_LENGTH -1){
+        ++PLAYER_POSITION[0];
+    }
+    
+}
+
+let Goright = () =>{
+    if(PLAYER_POSITION[1] < GRID_LENGTH - 1){
+        ++PLAYER_POSITION[1];
+    }
+    
+}
 
 Draw_Grid(GRID_LENGTH);
 Draw_Player(PLAYER_POSITION);
+
+addEventListener("keydown", function(e){
+    switch(e.keyCode){
+        case 37: //left
+            Goleft();
+            break;
+        case 38: //up
+            Goup();
+            break;
+        case 39: //right
+            Goright();
+            break;
+        case 40: //down
+            Godown();
+            break;
+    }
+    Draw_Player(PLAYER_POSITION);
+});
+
